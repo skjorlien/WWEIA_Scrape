84ab260c2b003896ee36badc2ef7984c16d5573f
skjorlien <skjorlien@gmail.com>
2025-03-18 15:54:24 -0700

    Incorporate more years of WWEIA

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -9,50 +9,91 @@ Each survey period is multi-year.
 
 Geographic information is restricted but available to those who have access. 
 
-I have "day" 
-
 '''
 
-files = [x for x in DATA_DIR.iterdir() if "interview_IF" in x.name]
+def generate_annual_IF_df(year):
+    '''
+    Not all participants are observed for both days. I take the average over the two days.
 
-day1_fname = "interview_IF_day1.xpt"
-day2_fname = "interview_IF_day2.xpt"
+    this function assumes you have downloaded the required datasets already
+    '''
+    
+    day1_fname = "interview_IF_day1.xpt"
+    day2_fname = "interview_IF_day2.xpt"
 
-rename_day1 = {
-    "SEQN": "i",
-    "DR1IFDCD": "j",
-    "DR1IGRMS": "grams"
+    rename_day1 = {
+        "SEQN": "i",
+        "DR1IFDCD": "j",
+        "DR1IGRMS": "grams"
     }
 
-day1 = pd.read_sas(DATA_DIR / day1_fname).rename(columns=rename_day1)[rename_day1.values()]
-day1 = day1.groupby(['i', 'j'])['grams'].sum().reset_index()
-day1["day"] = 1
+    day1 = pd.read_sas(DATA_DIR / year / day1_fname).rename(columns=rename_day1)[rename_day1.values()]
+    day1 = day1.groupby(['i', 'j'])['grams'].sum().reset_index()
+    day1["day"] = 1   
 
-
-rename_day2 = {
-    "SEQN": "i",
-    "DR2IFDCD": "j",
-    "DR2IGRMS": "grams"
+    rename_day2 = {
+        "SEQN": "i",
+        "DR2IFDCD": "j",
+        "DR2IGRMS": "grams"
     }
-day2 = pd.read_sas(DATA_DIR / day2_fname).rename(columns=rename_day2)[rename_day2.values()]
-day2 = day2.groupby(['i', 'j'])['grams'].sum().reset_index()
-day2["day"] = 2
+    day2 = pd.read_sas(DATA_DIR / year / day2_fname).rename(columns=rename_day2)[rename_day2.values()]
+    day2 = day2.groupby(['i', 'j'])['grams'].sum().reset_index()
+    day2["day"] = 2
+
+    ### Commenting out - price data is missing about a quarter of the food codes observed
+    # prices = pd.read_csv(DATA_DIR / "pp_national_average_prices.csv")
+    # prices = prices.loc[prices["year"].str.contains(year), ["food_code", "price_100gm"]]
+    # prices.columns = ["j", "price_100g"]
+    
+    df = pd.concat([day1, day2], ignore_index=True)
+    df["t"] = year
+    df["m"] = "USA"
+    df = df.groupby(["i", "j", "t", "m"])["grams"].mean().reset_index()
+
+    food_names = generate_annual_product_info(year)
+
+    df = df.merge(food_names, how="left", on=["j", "t"])
+    # df = df.merge(prices, how="left", on="j")
+    return df
 
 
-rename_demog = {
-    "SEQN": "i",
-    "RIDAGEYR": "age",
-    "RIAGENDR": "sex",
-    "RIDEXMON": "time"
-}
+def generate_annual_demog_df(year):
+    rename_demog = {
+        "SEQN": "i",
+        "RIDAGEYR": "age",
+        "RIAGENDR": "sex",
+        "DMDHHSIZ": "HH_size"
+    }
+    
+    demogs = pd.read_sas(DATA_DIR / year / "demographics.xpt").rename(columns=rename_demog)[rename_demog.values()]
+    demogs["m"] = "USA"
+    demogs["t"] = year
+    demogs["sex"] = demogs["sex"].astype(int).astype(str)
+    demogs["sex"] = demogs["sex"].map({"1": "Male", "2": "Female"})
+    return demogs.loc[:, ["i", "t", "m", "age", "sex", "HH_size"]]
 
-demogs = pd.read_sas(DATA_DIR / "demographics.xpt").rename(columns=rename_demog)[rename_demog.values()]
-df = pd.concat([day1, day2], ignore_index=True)
 
-product = pd.read_sas(DATA_DIR / "interview_support_file.xpt")
-product.columns = ["j", "del", "food_name"]
-product = product[["j", "food_name"]]
+def generate_annual_product_info(year):
+    product = pd.read_sas(DATA_DIR / year /  "interview_support_file.xpt")
+    product.columns = ["j", "food_name", "long_name"]
+    product["t"] = year
+    product["food_name"] = product["food_name"].str.decode('utf-8').str.capitalize()
+    return product[["j", "t", "food_name"]]
 
-df.to_csv(DATA_DIR /  "WWEIA_intake.csv", index=False)
-product.to_csv(DATA_DIR / "food_name_map.csv", index=False)
-demogs.to_csv(DATA_DIR / "demographics.csv", index=False)
+
+def generate_annual_dataproducts(year):
+    df = generate_annual_IF_df(year)
+    df.to_csv(DATA_DIR / "clean" / f"Food_Expenditures_{year}.csv", index=False)
+    
+    df = generate_annual_demog_df(year)
+    df.to_csv(DATA_DIR / "clean" / f"Household_Characteristics_{year}.csv", index=False)
+    
+    # df = generate_annual_product_info(year) 
+    # df.to_csv(DATA_DIR / "clean" / f"Product_names_{year}.csv", index=False)
+
+    
+if __name__ == "__main__":
+    years = ["2015", "2017", "2021"]
+    for y in years:
+        generate_annual_dataproducts(y)
+    
--- src/download_diet_data.py
+++ src/download_diet_data.py
@@ -5,34 +5,45 @@ from settings import DATA_DIR
 
 
 '''
-Script to download all WWEIA data in the 2021 - 2023 cycle. 
+Script to download all WWEIA diet data and demographics given some years. 
 
 There is an additional "variables_list" file that I manually saved as a csv.
 
+Link to the 2021 cycle:
 https://wwwn.cdc.gov/nchs/nhanes/search/DataPage.aspx?Component=Dietary&Cycle=2021-2023 
 '''
 
-files_to_download = {
+
+
+def download_data(link_dict, year):
+    for file, url in link_dict.items():
+        response = requests.get(url)
+
+        with open(DATA_DIR / year / file, "wb") as f:
+            f.write(response.content)
+
+# 2021 
+files_to_download_2021 = {
     "interview_IF_day1.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DR1IFF_L.xpt",
     "interview_IF_day2.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DR2IFF_L.xpt",
-    "interview_TNI_day1.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DR1TOT_L.xpt",
-    "interview_TNI_day2.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DR2TOT_L.xpt",
     "interview_support_file.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DRXFCD_L.xpt",
-    "blend_info.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/1999/DataFiles/DSBI.xpt",
-    "ingred_info.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/1999/DataFiles/DSII.xpt",
-    "product_info.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/1999/DataFiles/DSPI.xpt"
+    "demographics.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DEMO_L.xpt"
 }
 
-for file, url in files_to_download.items():
-    response = requests.get(url)
-    
-    with open(DATA_DIR / file, "wb") as f:
-        f.write(response.content)
+# 2017
+files_to_download_2017 = {
+    "interview_IF_day1.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/DR1IFF_J.xpt",
+    "interview_IF_day2.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/DR2IFF_J.xpt",
+    "interview_support_file.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/DRXFCD_J.xpt",
+    "demographics.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/DEMO_J.xpt"
+}
+        
+# 2015
+files_to_download_2015 = {
+    "interview_IF_day1.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2015/DataFiles/DR1IFF_I.xpt",
+    "interview_IF_day2.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2015/DataFiles/DR2IFF_I.xpt",
+    "interview_support_file.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2015/DataFiles/DRXFCD_I.xpt",
+    "demographics.xpt": "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2015/DataFiles/DEMO_I.xpt"
+}
 
-
-# also need demographics 
-url = "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DEMO_L.xpt"
-response = requests.get(url)
-
-with open(DATA_DIR / "demographics.xpt", "wb") as f:
-    f.write(response.content)
+download_data(files_to_download_2021, "2021")
